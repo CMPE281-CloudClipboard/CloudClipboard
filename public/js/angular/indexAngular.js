455f64d8c62610c656450e23196b8d552483dd55
gaurav51289 <gauravchodwadia@gmail.com>
2016-11-23 17:12:56 -0800

    Front end pages integrated in electron

---
+++ public/js/angular/indexAngular.js
@@ -0,0 +1,88 @@
+'use strict';
+var indexAngularApp = angular.module('indexAngularApp', ['ui.bootstrap', 'ui.router', 'ngCookies']);
+
+/**
+ * Route configuration for the RDash module.
+ */
+indexAngularApp.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        // For unmatched routes
+        $urlRouterProvider.otherwise('/');
+
+        // Application routes
+        $stateProvider
+            .state('index', {
+                url: '/',
+                templateUrl: '/clipboardhistory'
+            })
+            .state('clipboardhistory', {
+                url: '/clipboardhistory',
+                templateUrl: '/clipboardhistory'
+            })
+            .state('favourites', {
+                url: '/favourites',
+                templateUrl: '/favourites'
+            });
+    }
+]);
+
+
+/**
+ * Master Controller
+ */
+
+indexAngularApp.controller('indexCtrl', ['$scope', '$cookieStore', indexCtrl]);
+function indexCtrl($scope, $cookieStore) {
+    var mobileView = 992;
+
+    $scope.getWidth = function() {
+        return window.innerWidth;
+    };
+
+    $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+        if (newValue >= mobileView) {
+            if (angular.isDefined($cookieStore.get('toggle'))) {
+                $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
+            } else {
+                $scope.toggle = true;
+            }
+        } else {
+            $scope.toggle = false;
+        }
+
+    });
+
+    $scope.toggleSidebar = function() {
+        $scope.toggle = !$scope.toggle;
+        $cookieStore.put('toggle', $scope.toggle);
+    };
+
+    window.onresize = function() {
+        $scope.$apply();
+    };
+}
+
+
+//
+//
+// indexAngularApp.controller('AlertsCtrl', ['$scope', AlertsCtrl]);
+// function AlertsCtrl($scope) {
+//     $scope.alerts = [{
+//         type: 'success',
+//         msg: 'Thanks for visiting! Feel free to create pull requests to improve the dashboard!'
+//     }, {
+//         type: 'danger',
+//         msg: 'Found a bug? Create an issue with as many details as you can.'
+//     }];
+//
+//     $scope.addAlert = function() {
+//         $scope.alerts.push({
+//             msg: 'Another alert!'
+//         });
+//     };
+//
+//     $scope.closeAlert = function(index) {
+//         $scope.alerts.splice(index, 1);
+//     };
+// }
--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,22 @@
 "use strict";
 
 module.exports = function(app){
-  app.get('/', function(req, res){
+  app.get('/login', function(req, res){
     res.render('login', {});
   });
+
+  // app.get('/', function(req, res){
+  //   res.render('login', {});
+  // });
+
+  app.get('/', function(req, res){
+    res.render('index', {});
+  });
+  app.get('/clipboardhistory', function(req, res){
+    res.render('templates/clipboardhistory', {});
+  });
+  app.get('/favourites', function(req, res){
+    res.render('templates/favourites', {});
+  });
+
 };
