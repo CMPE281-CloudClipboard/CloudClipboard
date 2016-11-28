24d6cf0c2d57cdb22bd46cbf67fd848328c3cf91
gaurav51289 <gauravchodwadia@gmail.com>
2016-11-27 17:13:39 -0800

    Sign FrontEnd

--- routes/route.js
+++ routes/route.js
@@ -8,6 +8,9 @@ module.exports = function(app){
   app.get('/login', function(req, res){
     res.render('login', {});
   });
+  app.get('/signup', function(req, res){
+    res.render('signup', {});
+  });
   app.get('/', function(req, res){
     res.render('index', {});
   });
